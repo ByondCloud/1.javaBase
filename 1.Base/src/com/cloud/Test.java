409af0995771b0370181b5a2b03d5bef66afcfce
ByondCloud <1095938907@qq.com>
2022-07-14 11:59:58 +0800

    Initial commit

--- 1.Base/src/com/cloud/Test.java
+++ 1.Base/src/com/cloud/Test.java
@@ -1,25 +1,23 @@
 package com.cloud;
 
-import java.util.Scanner;
+
+import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 2; i >= 0; i--) {
-            String account = scanner.next();
-            if ("丁真".equals(account)) {
-                String pwd = scanner.next();
-                if ("666".equals(pwd)) {
-                    System.out.println("登录成功");
-                    break;
-                } else {
-                    System.out.println("密码错误，你还有" + i + "次机会");
+        int[] a = {1,8,5,7,3,9};
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
                 }
-                System.out.println("465456");
-            } else {
-                System.out.println("用户名错误，你还有" + i + "次机会");
             }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
         }
 
     }
